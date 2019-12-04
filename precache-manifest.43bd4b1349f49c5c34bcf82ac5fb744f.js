80d0c12c4afe82677ba1e8b0be4eaf9423f87d8d
sserichenko <sserichenko@gmail.com>
2019-12-04 13:22:36 +0200

    Updates

---
+++ precache-manifest.43bd4b1349f49c5c34bcf82ac5fb744f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b205e4fcd179764a22ae2adb07179f5",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "5ef0893fc71b57db72fb",
+    "url": "/goit-react-hw-04-movies/static/css/main.40a612c3.chunk.css"
+  },
+  {
+    "revision": "62bd12af7e63a29f155f",
+    "url": "/goit-react-hw-04-movies/static/js/2.e5fa8132.chunk.js"
+  },
+  {
+    "revision": "5ef0893fc71b57db72fb",
+    "url": "/goit-react-hw-04-movies/static/js/main.7f7c0496.chunk.js"
+  },
+  {
+    "revision": "38857a59c0410445e33b",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.652b3236.js"
+  }
+]);
